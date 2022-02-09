74eecf8c3fb06af7e64317f66f6cec12ac1cf9f5
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-02-09 17:16:22 +0000

    Adicionado pt à tag html

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="pt">
       <Head>
         <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <link
