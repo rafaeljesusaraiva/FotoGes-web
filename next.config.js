8d3cb9503c120c48cb3c044d51578fdadb89d530
Rafael de Jesus Saraiva <rafajs7@gmail.com>
2022-02-09 17:13:52 +0000

    Adicionado Build Estático

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const path = require('path');
 
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-};
+});
